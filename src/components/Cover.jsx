8e100b480b5fbde33248b7a00127aaa1ec51e3b8
NoemiP94 <m_emi94@hotmail.it>
2024-08-19 12:22:12 +0200

    add cover

---
+++ src/components/Cover.jsx
@@ -0,0 +1,50 @@
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import { MdOutlineFileDownload } from 'react-icons/md'
+
+const Cover = () => {
+  return (
+    <Container className="mt-4 ">
+      <Row className="flex flex-column flex-lg-row border-cover p-2 ">
+        <Col className="col-12 col-lg-4 flex flex-column text-center my-2 justify-content-center align-items-center ">
+          <div>img</div>
+          <div>
+            <Button>
+              Download CV <MdOutlineFileDownload className="color" />
+            </Button>
+          </div>
+        </Col>
+        <Col className="col-12 col-lg-8 my-1">
+          <p>
+            Sono una giovane Full Stack Developer:
+            <br />
+            creo applicativi web dinamici e funzionali a partire dallo sviluppo
+            dei database e API alla creazione di interfacce Web utilizzando i
+            metodi più efficaci e moderni per lo sviluppo appresi studiando
+            presso EPICODE.🌐​🛠️
+          </p>
+          <p>
+            ❤️ il mondo tech💻 soprattutto i video games. 🎮
+            <br />
+            ❤️ la "pop culture"📺 e la "Japan culture" 🎎, il fantasy🧙‍♂️, giocare
+            ai giochi di ruolo🎲, costruire LEGO 🧱 e puzzles🧩, leggere 📚,
+            ascoltare musica 🎶, cucire🧵e indossare i miei cosplay.👗
+          </p>
+          <p>
+            👥​Offro serietà, puntualità, impegno e voglia di imparare e
+            migliorarmi. Nel lavoro metto sempre tutta me stessa, mi piace
+            lavorare in team e confrontarmi con gli altri. Mi integro bene con i
+            team e sono sempre disposta ad aiutare se ne sono in grado e penso
+            di poter aggiungere un qualcosa in più con la persona che
+            sono.🤝​💼​
+          </p>{' '}
+          <p>
+            I miei valori fondamentali: equità, integrità, onestà, libertà e
+            rispetto. 🌈💖
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Cover
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import { Container } from 'react-bootstrap'
 import MyNav from './MyNav'
+import Cover from './Cover'
 
 const Home = () => {
   return (
     <Container className="mt-4">
       <MyNav />
+      <Cover />
     </Container>
   )
 }
