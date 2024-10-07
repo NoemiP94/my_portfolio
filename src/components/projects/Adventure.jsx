83baaa1e945e9dc2e381c0059656bf3e44658f82
NoemiP94 <m_emi94@hotmail.it>
2024-10-07 11:18:21 +0200

    update nika adventure

--- src/components/projects/Adventure.jsx
+++ src/components/projects/Adventure.jsx
@@ -2,6 +2,7 @@ import { Button, Col, Container, Row } from 'react-bootstrap'
 import adventure1 from '../../assets/adventure1.png'
 import adventure2 from '../../assets/adventure2.png'
 import adventure3 from '../../assets/adventure3.png'
+import adventure4 from '../../assets/adventure4.png'
 import { Link } from 'react-router-dom'
 import { FaGithub } from 'react-icons/fa'
 
@@ -12,18 +13,34 @@ const Adventure = () => {
         <Col className="color text-center fw-bold fs-2">
           <p>Nika&apos;s Adventure</p>
         </Col>
-        <Col className="d-flex  flex-row flex-wrap align-items-center justify-content-center">
-          <img src={adventure1} className="w-25 m-2" />
-          <img src={adventure2} className="w-25 m-2" />
-          <img src={adventure3} className="w-25 m-2" />
+        <Col className="d-flex flex-column flex-md-row flex-wrap align-items-center justify-content-center ">
+          <div className="d-flex  flex-md-row align-items-center justify-content-center mx-4">
+            <img src={adventure1} className="w-50 m-2 " />
+            <img src={adventure2} className="w-50 m-2" />
+          </div>
+          <div className="d-flex flex-md-row align-items-center justify-content-center mx-4">
+            {' '}
+            <img src={adventure3} className="w-50 m-2" />
+            <img src={adventure4} className="w-50 m-2" />
+          </div>
         </Col>
         <Col className="mt-5">
           <p>
-            Seguendo un tutorial sto sviluppando questo piccolo giochino in 2D.
-          </p>
+            Questo è un videogame 2D sviluppato in Java. Per questo progetto ho
+            seguito il tutorial di RyiSnow:{' '}
+            <span>
+              {' '}
+              <a
+                className="color text-decoration-none"
+                href="https://youtube.com/playlist?list=PL_QPQmz5C6WUF-pOQDsbsKbaBZqXj4qSq&si=A15KPYR4WoA_RULm"
+              >
+                &quot;How to Make a 2D Game in Java&quot;
+              </a>{' '}
+            </span>
+          </p>{' '}
           <p>
             Lo scopo è quello di imparare le meccaniche dietro il funzionamento
-            di un videogame.
+            di un videogame e aumentare la mia conoscenza su Java.
           </p>
         </Col>
         <Col>
