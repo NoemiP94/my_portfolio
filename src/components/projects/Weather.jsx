b9027fbff6e166ca1c619681a90bf3eb5b3f7815
NoemiP94 <m_emi94@hotmail.it>
2025-01-28 12:31:37 +0100

    add eng to weather

--- src/components/projects/Weather.jsx
+++ src/components/projects/Weather.jsx
@@ -2,8 +2,10 @@ import { Button, Col, Container, Row } from 'react-bootstrap'
 import weather from '../../assets/weather.png'
 import { Link } from 'react-router-dom'
 import { FaGithub } from 'react-icons/fa'
+import { useLanguage } from '../lang/LanguageProvider'
 
 const Weather = () => {
+  const { language } = useLanguage()
   return (
     <Container className="my-5 ">
       <Row className="border-cover p-2 flex flex-column">
@@ -13,14 +15,23 @@ const Weather = () => {
         <Col className="d-flex justify-content-center">
           <img src={weather} className="w-75" />
         </Col>
-        <Col className="mt-5">
-          <p>
-            Applicazione che permette la visualizzazione delle previsioni
-            meteorologiche.
-          </p>
-          <p>Per la realizzazione ho utilizzato una API esterna.</p>
-          <p className="color fw-bold">Responsive</p>
-        </Col>
+        {language === 'it' ? (
+          <Col className="mt-5">
+            <p>
+              Applicazione che permette la visualizzazione delle previsioni
+              meteorologiche.
+            </p>
+            <p>Per la realizzazione ho utilizzato una API esterna.</p>
+            <p className="color fw-bold">Responsive</p>
+          </Col>
+        ) : (
+          <Col className="mt-5">
+            <p>Application that allows to display weather forecasts.</p>
+            <p>For the realization I have used an external API.</p>
+            <p className="color fw-bold">Responsive</p>
+          </Col>
+        )}
+
         <Col>
           <p>Front-end:</p>
           <ul>
@@ -35,14 +46,22 @@ const Weather = () => {
             className="text-decoration-none color fw-bold p-2 d-flex align-items-center"
           >
             <FaGithub className="me-2" />
-            Vai alla repository{' '}
+            GitHub Repository{' '}
           </Link>
         </Col>
-        <Button className="w-50 m-3">
-          <Link to={'/'} className="text-decoration-none color">
-            Indietro
-          </Link>
-        </Button>
+        {language === 'it' ? (
+          <Button className="w-50 m-3">
+            <Link to={'/'} className="text-decoration-none color">
+              Indietro
+            </Link>
+          </Button>
+        ) : (
+          <Button className="w-50 m-3">
+            <Link to={'/'} className="text-decoration-none color">
+              Back
+            </Link>
+          </Button>
+        )}
       </Row>
     </Container>
   )
