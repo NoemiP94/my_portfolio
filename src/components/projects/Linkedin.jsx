3c04ea09029c14b8211c6a127e656e950818f1a0
NoemiP94 <m_emi94@hotmail.it>
2025-01-28 12:26:44 +0100

    add eng to linkedin

--- src/components/projects/Linkedin.jsx
+++ src/components/projects/Linkedin.jsx
@@ -2,8 +2,10 @@ import { Button, Col, Container, Row } from 'react-bootstrap'
 import linkedin from '../../assets/linkedin.png'
 import { Link } from 'react-router-dom'
 import { FaGithub } from 'react-icons/fa'
+import { useLanguage } from '../lang/LanguageProvider'
 
 const Linkedin = () => {
+  const { language } = useLanguage()
   return (
     <Container className="my-5 ">
       <Row className="border-cover p-2 flex flex-column">
@@ -13,18 +15,35 @@ const Linkedin = () => {
         <Col className="d-flex justify-content-center">
           <img src={linkedin} className="w-75" />
         </Col>
-        <Col className="mt-5">
-          <p>
-            Applicazione realizzata in team durante la terza Build Week del
-            corso di Epicode
-          </p>
-          <p>
-            Si tratta della riproduzione nella gran parte delle sue funzionalità
-            della famosa piattaforma per la ricerca del lavoro Linkedin.
-          </p>
+        {language === 'it' ? (
+          <Col className="mt-5">
+            <p>
+              Applicazione realizzata in team durante la terza Build Week del
+              corso di Epicode
+            </p>
+            <p>
+              Si tratta della riproduzione nella gran parte delle sue
+              funzionalità della famosa piattaforma per la ricerca del lavoro
+              Linkedin.
+            </p>
 
-          <p className="color fw-bold">Responsive</p>
-        </Col>
+            <p className="color fw-bold">Responsive</p>
+          </Col>
+        ) : (
+          <Col className="mt-5">
+            <p>
+              Application realized in team during the 3rd Build Week of
+              Epicode&apos;s class.
+            </p>
+            <p>
+              It&apos;s a reproduction in many of it&apos;s features of the
+              famous platform for job hunting Linkedin.
+            </p>
+
+            <p className="color fw-bold">Responsive</p>
+          </Col>
+        )}
+
         <Col>
           <p>Front-end:</p>
           <ul>
@@ -39,14 +58,22 @@ const Linkedin = () => {
             className="text-decoration-none color fw-bold p-2 d-flex align-items-center"
           >
             <FaGithub className="me-2" />
-            Vai alla repository{' '}
+            GitHub Repository{' '}
           </Link>
         </Col>
-        <Button className="w-50 m-3">
-          <Link to={'/'} className="text-decoration-none color">
-            Indietro
-          </Link>
-        </Button>
+        {language === 'it' ? (
+          <Button className="w-50 m-3">
+            <Link to={'/'} className="text-decoration-none color">
+              Indietro
+            </Link>
+          </Button>
+        ) : (
+          <Button className="w-50 m-3">
+            <Link to={'/'} className="text-decoration-none color">
+              Back
+            </Link>
+          </Button>
+        )}
       </Row>
     </Container>
   )
